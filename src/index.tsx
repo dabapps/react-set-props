769645b255b3b9ff7b1e6d0c521ab027a35e76f3
Jake 'Sid' Smith <jake@dabapps.com>
2018-09-26 09:31:01 +0100

    Import Dispatch from redux (not available in some react-redux-types)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { connect, Dispatch } from 'react-redux';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 import { v4 as uuid } from 'uuid';
 
 export const STORE_KEY = 'setPropsReducer';
